fb1f6c2b29bba0cde38dc041d446eaf2c931e8e1
Vikas Yadav <yadavvikas859@gmail.com>
2021-08-31 08:28:33 +0530

    updated permissions

--- blogapi/blog_project/posts/views.py
+++ blogapi/blog_project/posts/views.py
@@ -5,11 +5,13 @@ from .serializers import PostSerializer
 from .permissions import IsAuthorOrReadOnly
 
 class PostList(generics.ListCreateAPIView):
-    permission_classes = (IsAuthorOrReadOnly,)
+    #permission_classes = (permissions.IsAuthenticated,) 
+    #permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    #permission_classes = (permissions.IsAuthenticated,)
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
