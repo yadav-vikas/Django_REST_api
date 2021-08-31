0a455a663d17e38c26b358792416de9e431b9464
Vikas Yadav <yadavvikas859@gmail.com>
2021-09-01 04:34:20 +0530

    added routers to viewsets

--- blogapi/blog_project/posts/views.py
+++ blogapi/blog_project/posts/views.py
@@ -4,23 +4,34 @@ from rest_framework import generics,permissions
 from .models import Post
 from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
+from rest_framework import viewsets
 
-class PostList(generics.ListCreateAPIView):
-    #permission_classes = (permissions.IsAuthenticated,) 
-    #permission_classes = (IsAuthorOrReadOnly,)
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+# class PostList(generics.ListCreateAPIView):
+#     #permission_classes = (permissions.IsAuthenticated,) 
+#     #permission_classes = (IsAuthorOrReadOnly,)
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    #permission_classes = (permissions.IsAuthenticated,)
+# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+#     #permission_classes = (permissions.IsAuthenticated,)
+#     permission_classes = (IsAuthorOrReadOnly,)
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+    
+# class UserList(generics.ListCreateAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
+
+# class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = PostSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    
-class UserList(generics.ListCreateAPIView):
+
+class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = PostSerializer
+    
