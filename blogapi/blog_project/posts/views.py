8c06159010c9722ac3f7688658500545b5e26265
Vikas Yadav <yadavvikas859@gmail.com>
2021-09-01 04:22:37 +0530

    User viewsets

--- blogapi/blog_project/posts/views.py
+++ blogapi/blog_project/posts/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from rest_framework import generics,permissions
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
 
 class PostList(generics.ListCreateAPIView):
@@ -16,3 +17,10 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = PostSerializer
